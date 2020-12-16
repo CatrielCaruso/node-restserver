605f2f0c3f5bac5d7222f593853a7072520248e5
Cat <catrielcaruso@gmail.com>
2020-12-16 16:28:44 -0300

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,32 @@
 //=================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//=================
+// ENTORNO
+//=================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=================
+// BASES DE DATOS
+//=================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+
+    urlDB = 'mongodb://localhost:27017/cafe';
+
+
+} else {
+
+
+    urlDB = 'mongodb+srv://catriel:F0OaDLE09sJlrpvu@cluster0.bdtpc.mongodb.net/cafe';
+
+
+
+}
+
+process.env.URLDB = urlDB;
